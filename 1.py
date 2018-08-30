6a939ac1d5e170d2be9b9478306066a9094b4283
wjh <735008042@qq.com>
2018-08-30 21:30:41 +0800

    frist commit 第一次提交
    Signed-off-by: wjh <735008042@qq.com>

---
+++ 1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Agian"
+print "I like typing this."
+print "this is fun"
+print 'Yay!Printing.'
+print "i'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 
